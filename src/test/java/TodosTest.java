c9eb2ac035cab522c5ca1f9f78a2c7e4a4525bf1
Sergey Bykhavets <sergozby@gamil.com>
2024-09-29 21:17:39 +0300

    Improved test for Task

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -127,6 +127,29 @@ public class TodosTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void shouldNotFindAny() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Солнце");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
     // FOR STATISTIC ONLY
     // Task
     // getId from Task
